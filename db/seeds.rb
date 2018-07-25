99333e1589467aa04d5b4189ebd5ea8f9c6620a0
Drwhirstyburgers <dave.hirst50@gmail.com>
2018-07-25 17:24:57 -0400

    seed file updated

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,18 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+10.times do
+  User.create(
+    username: Faker::Name.unique.name,
+    email: Faker::Internet.unique.email,
+    confirmed_at: Time.now,
+    password: 'password'
+  )
+  users = User.all
+
+  100.times do
+    Item.create(
+      user: users.sample,
+      name: Faker::MichaelScott.quote
+    )
+  end
+end
