7e62175b0513cef3acb3a92c21b75c8758842ef7
Drwhirstyburgers <dave.hirst50@gmail.com>
2018-07-28 09:06:03 -0400

    got rid of flash notifications for items save/destroy

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -3,12 +3,7 @@ class ItemsController < ApplicationController
   def create
     @item = current_user.items.build(item_params)
     @new_item = Item.new
-
-    if @item.save
-      flash[:notice] = "Item was added to the list"
-    else
-      flash[:error] = "There was an error adding the item. Please try again."
-    end
+    @item.save
 
     respond_to do |format|
       format.html
